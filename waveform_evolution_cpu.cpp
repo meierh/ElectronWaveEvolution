5fe57709304a8fcd9fe22fe3e0fd26ea18fc3ae9
Philipp <p.ziser51@gmail.com>
2025-01-11 16:56:17 +0100

    Performance increases for cpu: Used hashset and vector etc.

--- waveform_evolution_cpu.cpp
+++ waveform_evolution_cpu.cpp
@@ -1,21 +1,26 @@
 #include <waveform_evolution_cpu.hpp>
 
+#include <vector>
+#include <unordered_set>
+
 // TODO: remove printf in this file
 cuda::std::pair<pmpp::cuda_ptr<std::uint64_t[]>, std::size_t> evolve_operator_cpu(
 	cuda::std::span<std::uint64_t const> device_wavefunction,
 	std::uint64_t activation, std::uint64_t deactivation
 )
 {
-    const std::uint64_t* wave_data = device_wavefunction.data();
-
+    std::vector<uint64_t> wave_data_vector_cpu(device_wavefunction.size());
+    cudaMemcpy(wave_data_vector_cpu.data(), device_wavefunction.data(), sizeof(std::uint64_t) * device_wavefunction.size(), cudaMemcpyDeviceToHost);
+    uint64_t size = device_wavefunction.size();
     // to save the new waves
-    // pmpp::cuda_ptr<std::uint64_t[]> wave_added = pmpp::make_managed_cuda_array<std::uint64_t>(maxOffset,cudaMemAttachGlobal,&allocError);
     uint64_t wave_added_size = 0;
+    // to check for dupicated values in constant time
+    std::unordered_set<uint64_t> wave_data_set_cpu(wave_data_vector_cpu.begin(), wave_data_vector_cpu.end());
 
     // get number of new waves
-    for(uint64_t wave_data_index = 0; wave_data_index<device_wavefunction.size(); wave_data_index++)
+    for(uint64_t wave_data_index = 0; wave_data_index<size; wave_data_index++)
     {
-        std::uint64_t wave = wave_data[wave_data_index];
+        std::uint64_t wave = wave_data_vector_cpu[wave_data_index];
         // check for collision
         bool col = (bool)((wave & activation) | ((~wave) & deactivation));
         if(!col)
@@ -26,89 +31,21 @@ cuda::std::pair<pmpp::cuda_ptr<std::uint64_t[]>, std::size_t> evolve_operator_cp
 			new_wave &= ~deactivation;
 
             // check whether the value is a duplicate
-            bool isDuplicate = false;
-
-            // duplicate_index works like wave_data_index
-            for(uint64_t duplicate_index = 0; duplicate_index<device_wavefunction.size(); duplicate_index++)
+            if(!(wave_data_set_cpu.contains(new_wave)))
             {
-                if(new_wave == wave_data[duplicate_index])
-               {
-                    isDuplicate = true;
-                    break;
-                }
-            }
-            if(isDuplicate == false)
-            {
-                wave_added_size++;
+                // add the new wave if it is no dupicate 
+                wave_data_vector_cpu.push_back(new_wave);
             }
         }
     }
-    cudaError_t allocError;
 
-    // to save the new waves
-    pmpp::cuda_ptr<std::uint64_t[]> wave_added = pmpp::make_managed_cuda_array<std::uint64_t>(wave_added_size,cudaMemAttachGlobal,&allocError);
-
-    // needed since dynamic sized data structures are difficult in c++
-    uint64_t wave_added_index = 0;
-
-    // almost the same loop again to save the values
-    for(uint64_t wave_data_index = 0; wave_data_index<device_wavefunction.size(); wave_data_index++)
-    {
-        //std::printf("Iteration: %ld, ",wave_data_index);
-        std::uint64_t wave = wave_data[wave_data_index];
-        //std::printf("Input: %ld, ",wave);
-        // check for collision
-        bool col = (bool)((wave & activation) | ((~wave) & deactivation));
-        //std::printf("col: %d\n",col);
-        if(!col)
-        {
-            // apply activation and deactivation operator
-            std::uint64_t new_wave = wave;
-			new_wave |= activation;
-			new_wave &= ~deactivation;
-            // check whether the value is a duplicate
-            bool isDuplicate = false;
-
-            // duplicate_index works like wave_data_index
-            for(uint64_t duplicate_index = 0; duplicate_index<device_wavefunction.size(); duplicate_index++)
-            {
-                if(new_wave == wave_data[duplicate_index])
-               {
-                    isDuplicate = true;
-                    //std::printf("duplicate\n");
-                    break;
-                }
-            }
-            if(isDuplicate == false)
-            {
-                //std::printf("Added %ld\n",new_wave);
-                wave_added[wave_added_index] = new_wave;
-                wave_added_index++;
-                //std::printf("wave_added: ");
-				/*
-                for(int i = 0; i<wave_added_size;i++)
-                {
-                    std::printf("%ld ",wave_added[i]);
-                }
-                std::printf("\n");
-                */
-            }
-        }
-    }
-    /*
-    std::printf("wave_added: ");
-    for(int i = 0; i<wave_added_size;i++)
-    {
-        std::printf("%ld ",wave_added[i]);
-    }
-    std::printf("\n");
-    */
     // set return value
+    cudaError_t allocError;
     cuda::std::pair<pmpp::cuda_ptr<std::uint64_t[]>, std::size_t> waveOut;
-    waveOut.second = device_wavefunction.size()+wave_added_size;
+    waveOut.second = wave_data_vector_cpu.size();
     waveOut.first = pmpp::make_managed_cuda_array<std::uint64_t>
     (
-        device_wavefunction.size()+wave_added_size,
+        wave_data_vector_cpu.size(),
         cudaMemAttachGlobal,
         &allocError
     );
@@ -116,20 +53,10 @@ cuda::std::pair<pmpp::cuda_ptr<std::uint64_t[]>, std::size_t> evolve_operator_cp
     cudaMemcpy
     (
         waveOut.first.get(),
-        device_wavefunction.data(),
-        device_wavefunction.size()*sizeof(std::uint64_t),
-        cudaMemcpyHostToHost
+        wave_data_vector_cpu.data(),
+        wave_data_vector_cpu.size()*sizeof(std::uint64_t),
+        cudaMemcpyHostToDevice
     );
-    if(wave_added_size>0)
-    {
-        cudaMemcpy
-        (
-            waveOut.first.get()+device_wavefunction.size(),
-            wave_added.get(),
-            wave_added_size*sizeof(std::uint64_t),
-            cudaMemcpyHostToHost
-        );
-    }
     return waveOut;
 }
 
@@ -139,7 +66,6 @@ cuda::std::pair<pmpp::cuda_ptr<std::uint64_t[]>, std::size_t> evolve_ansatz_cpu(
 	cuda::std::span<std::uint64_t const> deactivations
 )
 {
-	/* TODO */
 	cuda::std::pair<pmpp::cuda_ptr<std::uint64_t[]>, std::size_t> result;
 	for(std::uint64_t operatorInd=0; operatorInd<activations.size(); operatorInd++)
 	{
